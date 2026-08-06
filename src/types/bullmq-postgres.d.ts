379cbea080a9bbf6d87262def2a73648aa98b902
chandiwalaamaan-droid <chandiwalaamaan@gmail.com>
2026-08-07 01:10:28 +0530

    Delete src directory

--- src/types/bullmq-postgres.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "bullmq/postgres" {
-  export function createPostgresBackend(...args: any[]): any;
-  export class PostgresConnection extends require("events").EventEmitter {
-    constructor(connection: any);
-    waitUntilReady(): Promise<void>;
-    close(): Promise<void>;
-  }
-  export type PostgresConnectionOptions = any;
-}
